b002d7b8f88eabf102ff11eca1089df0117e59ea
bjbivins <bjbivins@fullsail.edu>
2019-12-18 23:25:25 -0600

    CodeAdded

---
+++ BivinsBraxton_PaintShelf/BivinsBraxton_PaintShelf/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace BivinsBraxton_PaintShelf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RedButton = new System.Windows.Forms.Button();
+            this.BlueButton = new System.Windows.Forms.Button();
+            this.GreenButton = new System.Windows.Forms.Button();
+            this.YellowButton = new System.Windows.Forms.Button();
+            this.PinkButton = new System.Windows.Forms.Button();
+            this.BlackButton = new System.Windows.Forms.Button();
+            this.GrayButton = new System.Windows.Forms.Button();
+            this.SilverButton = new System.Windows.Forms.Button();
+            this.OrangeButton = new System.Windows.Forms.Button();
+            this.PurpleButton = new System.Windows.Forms.Button();
+            this.WhiteButton = new System.Windows.Forms.Button();
+            this.SpecialButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPaintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newPaintToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RedButton
+            // 
+            this.RedButton.BackColor = System.Drawing.Color.Red;
+            this.RedButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.RedButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RedButton.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.RedButton.Location = new System.Drawing.Point(12, 67);
+            this.RedButton.Name = "RedButton";
+            this.RedButton.Size = new System.Drawing.Size(130, 72);
+            this.RedButton.TabIndex = 0;
+            this.RedButton.Text = "RED";
+            this.RedButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.RedButton.UseVisualStyleBackColor = false;
+            this.RedButton.Click += new System.EventHandler(this.RedButton_Click);
+            // 
+            // BlueButton
+            // 
+            this.BlueButton.BackColor = System.Drawing.Color.Blue;
+            this.BlueButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BlueButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BlueButton.ForeColor = System.Drawing.Color.Transparent;
+            this.BlueButton.Location = new System.Drawing.Point(148, 67);
+            this.BlueButton.Name = "BlueButton";
+            this.BlueButton.Size = new System.Drawing.Size(130, 72);
+            this.BlueButton.TabIndex = 1;
+            this.BlueButton.Text = "BLUE";
+            this.BlueButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.BlueButton.UseVisualStyleBackColor = false;
+            this.BlueButton.Click += new System.EventHandler(this.BlueButton_Click);
+            // 
+            // GreenButton
+            // 
+            this.GreenButton.BackColor = System.Drawing.Color.Lime;
+            this.GreenButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.GreenButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GreenButton.Location = new System.Drawing.Point(12, 145);
+            this.GreenButton.Name = "GreenButton";
+            this.GreenButton.Size = new System.Drawing.Size(130, 72);
+            this.GreenButton.TabIndex = 2;
+            this.GreenButton.Text = "GREEN";
+            this.GreenButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.GreenButton.UseVisualStyleBackColor = false;
+            this.GreenButton.Click += new System.EventHandler(this.GreenButton_Click);
+            // 
+            // YellowButton
+            // 
+            this.YellowButton.BackColor = System.Drawing.Color.Yellow;
+            this.YellowButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.YellowButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.YellowButton.Location = new System.Drawing.Point(148, 145);
+            this.YellowButton.Name = "YellowButton";
+            this.YellowButton.Size = new System.Drawing.Size(130, 72);
+            this.YellowButton.TabIndex = 3;
+            this.YellowButton.Text = "YELLOW";
+            this.YellowButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.YellowButton.UseVisualStyleBackColor = false;
+            this.YellowButton.Click += new System.EventHandler(this.YellowButton_Click);
+            // 
+            // PinkButton
+            // 
+            this.PinkButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.PinkButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.PinkButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PinkButton.Location = new System.Drawing.Point(12, 223);
+            this.PinkButton.Name = "PinkButton";
+            this.PinkButton.Size = new System.Drawing.Size(130, 72);
+            this.PinkButton.TabIndex = 4;
+            this.PinkButton.Text = "PINK";
+            this.PinkButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.PinkButton.UseVisualStyleBackColor = false;
+            this.PinkButton.Click += new System.EventHandler(this.PinkButton_Click);
+            // 
+            // BlackButton
+            // 
+            this.BlackButton.BackColor = System.Drawing.Color.Black;
+            this.BlackButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.BlackButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BlackButton.ForeColor = System.Drawing.Color.Transparent;
+            this.BlackButton.Location = new System.Drawing.Point(148, 223);
+            this.BlackButton.Name = "BlackButton";
+            this.BlackButton.Size = new System.Drawing.Size(130, 72);
+            this.BlackButton.TabIndex = 5;
+            this.BlackButton.Text = "BLACK";
+            this.BlackButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.BlackButton.UseVisualStyleBackColor = false;
+            this.BlackButton.Click += new System.EventHandler(this.BlackButton_Click);
+            // 
+            // GrayButton
+            // 
+            this.GrayButton.BackColor = System.Drawing.Color.Gray;
+            this.GrayButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.GrayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GrayButton.ForeColor = System.Drawing.Color.White;
+            this.GrayButton.Location = new System.Drawing.Point(12, 301);
+            this.GrayButton.Name = "GrayButton";
+            this.GrayButton.Size = new System.Drawing.Size(130, 72);
+            this.GrayButton.TabIndex = 6;
+            this.GrayButton.Text = "GRAY";
+            this.GrayButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.GrayButton.UseVisualStyleBackColor = false;
+            this.GrayButton.Click += new System.EventHandler(this.GrayButton_Click);
+            // 
+            // SilverButton
+            // 
+            this.SilverButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.SilverButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.SilverButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SilverButton.Location = new System.Drawing.Point(148, 301);
+            this.SilverButton.Name = "SilverButton";
+            this.SilverButton.Size = new System.Drawing.Size(130, 72);
+            this.SilverButton.TabIndex = 7;
+            this.SilverButton.Text = "SILVER";
+            this.SilverButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.SilverButton.UseVisualStyleBackColor = false;
+            this.SilverButton.Click += new System.EventHandler(this.SilverButton_Click);
+            // 
+            // OrangeButton
+            // 
+            this.OrangeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.OrangeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.OrangeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OrangeButton.Location = new System.Drawing.Point(12, 379);
+            this.OrangeButton.Name = "OrangeButton";
+            this.OrangeButton.Size = new System.Drawing.Size(130, 72);
+            this.OrangeButton.TabIndex = 8;
+            this.OrangeButton.Text = "ORANGE";
+            this.OrangeButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.OrangeButton.UseVisualStyleBackColor = false;
+            this.OrangeButton.Click += new System.EventHandler(this.OrangeButton_Click);
+            // 
+            // PurpleButton
+            // 
+            this.PurpleButton.BackColor = System.Drawing.Color.Purple;
+            this.PurpleButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.PurpleButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PurpleButton.ForeColor = System.Drawing.Color.Transparent;
+            this.PurpleButton.Location = new System.Drawing.Point(148, 379);
+            this.PurpleButton.Name = "PurpleButton";
+            this.PurpleButton.Size = new System.Drawing.Size(130, 72);
+            this.PurpleButton.TabIndex = 9;
+            this.PurpleButton.Text = "PURPLE";
+            this.PurpleButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.PurpleButton.UseVisualStyleBackColor = false;
+            this.PurpleButton.Click += new System.EventHandler(this.PurpleButton_Click);
+            // 
+            // WhiteButton
+            // 
+            this.WhiteButton.BackColor = System.Drawing.Color.White;
+            this.WhiteButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.WhiteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.WhiteButton.Location = new System.Drawing.Point(12, 457);
+            this.WhiteButton.Name = "WhiteButton";
+            this.WhiteButton.Size = new System.Drawing.Size(130, 72);
+            this.WhiteButton.TabIndex = 10;
+            this.WhiteButton.Text = "WHITE";
+            this.WhiteButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.WhiteButton.UseVisualStyleBackColor = false;
+            this.WhiteButton.Click += new System.EventHandler(this.WhiteButton_Click);
+            // 
+            // SpecialButton
+            // 
+            this.SpecialButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.SpecialButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SpecialButton.Location = new System.Drawing.Point(148, 457);
+            this.SpecialButton.Name = "SpecialButton";
+            this.SpecialButton.Size = new System.Drawing.Size(130, 72);
+            this.SpecialButton.TabIndex = 11;
+            this.SpecialButton.Text = "SPECIAL";
+            this.SpecialButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.SpecialButton.UseVisualStyleBackColor = true;
+            this.SpecialButton.Click += new System.EventHandler(this.SpecialButton_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.addPaintToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(291, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // addPaintToolStripMenuItem
+            // 
+            this.addPaintToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newPaintToolStripMenuItem});
+            this.addPaintToolStripMenuItem.Name = "addPaintToolStripMenuItem";
+            this.addPaintToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.addPaintToolStripMenuItem.Text = "Add";
+            // 
+            // newPaintToolStripMenuItem
+            // 
+            this.newPaintToolStripMenuItem.Name = "newPaintToolStripMenuItem";
+            this.newPaintToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newPaintToolStripMenuItem.Text = "New Paint";
+            this.newPaintToolStripMenuItem.Click += new System.EventHandler(this.newPaintToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 543);
+            this.Controls.Add(this.SpecialButton);
+            this.Controls.Add(this.WhiteButton);
+            this.Controls.Add(this.PurpleButton);
+            this.Controls.Add(this.OrangeButton);
+            this.Controls.Add(this.SilverButton);
+            this.Controls.Add(this.GrayButton);
+            this.Controls.Add(this.BlackButton);
+            this.Controls.Add(this.PinkButton);
+            this.Controls.Add(this.YellowButton);
+            this.Controls.Add(this.GreenButton);
+            this.Controls.Add(this.BlueButton);
+            this.Controls.Add(this.RedButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RedButton;
+        private System.Windows.Forms.Button BlueButton;
+        private System.Windows.Forms.Button GreenButton;
+        private System.Windows.Forms.Button YellowButton;
+        private System.Windows.Forms.Button PinkButton;
+        private System.Windows.Forms.Button BlackButton;
+        private System.Windows.Forms.Button GrayButton;
+        private System.Windows.Forms.Button SilverButton;
+        private System.Windows.Forms.Button OrangeButton;
+        private System.Windows.Forms.Button PurpleButton;
+        private System.Windows.Forms.Button WhiteButton;
+        private System.Windows.Forms.Button SpecialButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPaintToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newPaintToolStripMenuItem;
+    }
+}
+
---
+++ BivinsBraxton_PaintShelf/BivinsBraxton_PaintShelf/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace BivinsBraxton_PaintShelf
+{
+    public partial class Form1 : Form
+    {
+        WebClient apiConnection = new WebClient();
+        List<data> dataList = new List<data>();
+        MySqlConnection conn = new MySqlConnection();
+        DataTable theData = new DataTable();
+        string connectionString = "";
+        int row = 0;
+        int maxCount = 0;
+        string uid = "dbremoteuser"; //"dbsAdmin";
+        string dbs = "paintshelf"; //"exampleDatabase";
+        string pas = "password";
+
+        /// SKIZMO ///
+        // FAKE //
+        // http://vinapi.skizmo.com/vins/ 
+
+        /// FORTELLIS ///
+        // WAITING FOR AUTH CODE // DOESNT RETURN PAINT FROM VIN // 
+        // https://api.fortellis.io/service/reference/v4/vehicle-specifications/vins/{vin} 
+        // WAITING FOR AUTH CODE // ONLY CARS BEING SERVICED // 
+        // https://developer.fortellis.io/api-reference/vehicle-service/service-vehicle-specifications-api
+        // WAITING FOR AUTH CODE // ONLY CARS BEING SOLD // 
+        // https://api.fortellis.io/sales/inventory/v2/merchandisable-vehicles/
+
+        /// NHTSA ///
+        // https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/5UXWX7C5*BA?format=xml&modelyear=2011
+
+
+        string startingAPI = "https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/";
+        string midAPI = "?format=xml&modelyear=";
+        string VIN_API = "5UXWX7C5*BA";
+        string YEAR_API = "2011";
+        string apiEndPoint;
+        string make;
+        string model;
+
+        private string BuildConnectionString(string database, string uid, string pword)
+        {
+            string serverIP = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\VFW\\connect.txt"))
+                {
+                    serverIP = sr.ReadToEnd();
+                }
+                string prt = "3306";
+                return "server=" + serverIP + ";uid=" + uid +
+                    ";pwd=" + pword + ";database=" + database + ";port=" + prt;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return "ERROR";
+            }
+        }
+
+        private void Connect(string myConnectionString, string database)
+        {
+            try
+            {
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+               // connectStatus.Text = "Connected to: " + database;
+            }
+
+            catch (MySqlException e)
+            {
+                string msg = "";
+
+                switch (e.Number)
+                {
+                    case 0:
+                        {
+                            msg = e.ToString();
+                            break;
+                        }
+
+                    case 1042:
+                        {
+                            msg = "Can't Resolve Host Address.\n" + myConnectionString;
+                            break;
+                        }
+
+                    case 1045:
+                        {
+                            msg = "Invalid Username or Password";
+                            break;
+                        }
+
+                    default:
+                        {
+                            msg = e.ToString() + "\n" + myConnectionString;
+                            break;
+                        }
+                }
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void BuildAPI()
+        {
+            apiEndPoint = startingAPI + VIN_API + midAPI + YEAR_API;
+        }
+
+        private void ReadFromAPI()
+        {
+            make = "";
+            model = "";
+            //string pnt_Code = "";
+            // string pnt_Name = "";
+
+            using (XmlReader apiData = XmlReader.Create(apiEndPoint))
+            {
+                while(apiData.Read())
+                {
+                    if (apiData.Name == "Make")
+                    {
+                        make = apiData.ReadElementContentAsString();
+                    }
+
+                    if (apiData.Name == "Model")
+                    {
+                        model = apiData.ReadElementContentAsString();
+                    }
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = BuildConnectionString(dbs, uid, pas);
+            Connect(connectionString, dbs);
+            //BuildAPI();
+            //ReadFromAPI();
+
+           // MessageBox.Show("Make: " + make + "Model: " + model);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void SelectColors(int cid)
+        {
+            string completeString = "No Paint stored";
+            string sql = "SELECT Make, Year, PaintCode, PaintName FROM paints where ColorID = " + cid.ToString();
+
+            try
+            {
+                MySqlDataAdapter adr = new MySqlDataAdapter(sql, conn);
+                adr.SelectCommand.CommandType = CommandType.Text;
+                adr.Fill(theData);
+                int numberOfRecords = theData.Select().Length;
+                if (numberOfRecords > 0)
+                {
+                    completeString = "";
+                    for (int i = 0; i < numberOfRecords; i++)
+                    {
+                        string make = theData.Rows[i]["Make"].ToString();
+                        int year = 0;
+                        int.TryParse(theData.Rows[i]["Year"].ToString(), out year);
+                        string paintCode = theData.Rows[i]["PaintCode"].ToString();
+                        string paintName = theData.Rows[i]["PaintName"].ToString();
+                        maxCount = numberOfRecords;
+
+                        completeString += "Make = " + make + "\n" +
+                                                "Year = " + year + "\n" +
+                                                "Paint Code = " + paintCode + "\n" +
+                                                "Paint Name = " + paintName + "\n\n";
+                    }
+                }
+            }
+
+            catch
+            {
+                completeString = "Connection to Database Failed";
+            }
+            MessageBox.Show(completeString);
+        }
+
+        private void RedButton_Click(object sender, EventArgs e)
+        {
+            // 1 RED //
+            SelectColors(1);
+        }
+
+        private void BlueButton_Click(object sender, EventArgs e)
+        {
+            // 2 BLUE //
+            SelectColors(2);
+        }
+
+        private void GreenButton_Click(object sender, EventArgs e)
+        {
+            // 3 GREEN //
+            SelectColors(3);
+        }
+
+        private void YellowButton_Click(object sender, EventArgs e)
+        {
+            // 4 YELLOW //
+            SelectColors(4);
+        }
+
+        private void PinkButton_Click(object sender, EventArgs e)
+        {
+            // 5 PINK //
+            SelectColors(5);
+        }
+
+        private void BlackButton_Click(object sender, EventArgs e)
+        {
+            // 6 BLACK //
+            SelectColors(6);
+        }
+
+        private void GrayButton_Click(object sender, EventArgs e)
+        {
+            // 7 GRAY //
+            SelectColors(7);
+        }
+
+        private void SilverButton_Click(object sender, EventArgs e)
+        {
+            // 8 SILVER //
+            SelectColors(8);
+        }
+
+        private void OrangeButton_Click(object sender, EventArgs e)
+        {
+            // 9 ORANGE //
+            SelectColors(9);
+        }
+
+        private void PurpleButton_Click(object sender, EventArgs e)
+        {
+            // 10 PURPLE //
+            SelectColors(10);
+        }
+
+        private void WhiteButton_Click(object sender, EventArgs e)
+        {
+            // 11 WHITE //
+            SelectColors(11);
+        }
+
+        private void SpecialButton_Click(object sender, EventArgs e)
+        {
+            // 12 SPECIAL //
+            SelectColors(12);
+        }
+
+        public void AddNewPaint(string make, int year, string paintCode, string paintName, int cid)
+        {
+            string sqlString = "insert into Paints ( Make, Year, PaintCode, PaintName, ColorID) values ('" + 
+                make + "', " + year + ", '" + paintCode + "', '" + paintName + "', " + cid + ")";
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            Connect(connectionString, dbs);
+            try
+            {
+                using (MySqlCommand comm = new MySqlCommand(sqlString, conn))
+                {
+                    comm.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void newPaintToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string make = "Dodge";
+            int year = 2017;
+            string paintCode = "PR4";
+            string paintName = "Hot Rod Red";
+            int colorId = 1;
+
+            AddNewPaint(make, year, paintCode, paintName, colorId);
+        }
+    }
+}
